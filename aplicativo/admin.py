ea2a6d705cbf9ac3d4c2db9c712c7ac75c1ee576
Marília <marilia.gabriella@escolar.ifrn.edu.br>
2024-11-14 17:43:25 -0300

    Criação de modelos de tabelas

--- aplicativo/admin.py
+++ aplicativo/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from aplicativo.models import Car
 
-# Register your models here.
-admin.site.register(Car)
+class CarFilter(admin.ModelAdmin):
+    list_display = ("id", "model", "brand", "year") 
+    #nome dos campos dos modelos que estavam nas linhas
+    list_display_links = ("id", "model", "brand", "year")
+    list_filter = ("model", "year")
+    search_fields = ("year", "model", "brand")
+
+admin.site.register(Car, CarFilter)
