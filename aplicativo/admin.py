9be49f395340b2cc13df638040ab2dea32cbf670
Marília <marilia.gabriella@escolar.ifrn.edu.br>
2024-11-22 16:05:23 -0300

    Adc user logado e ao modelo de cars

--- aplicativo/admin.py
+++ aplicativo/admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from aplicativo.models import Car
 
 class CarFilter(admin.ModelAdmin):
-    list_display = ("id", "model", "brand", "year") 
+    list_display = ("id","user", "model", "brand", "year") 
     #nome dos campos dos modelos que estavam nas linhas
-    list_display_links = ("id", "model", "brand", "year")
-    list_filter = ("model", "year")
-    search_fields = ("year", "model", "brand")
+    list_display_links = ("id", "model", "brand", "year") #tranforma em links
+    list_filter = ("model", "year") #filtra
+    search_fields = ("year", "model", "brand") #cria a caixa de pesquisa
 
-admin.site.register(Car, CarFilter)
+admin.site.register(Car, CarFilter) #tem q ser registrado o filter para funcionar
